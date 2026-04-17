8ed4959f0780809d440d23ba524f81b921ce4be9
nicolzapata <zapatanicol094@gmail.com>
2026-04-16 23:03:20 -0500

    url no local

--- sportwear/src/services/api.js
+++ sportwear/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://surprising-ambition-production-dd12.up.railway.app/api",
   headers: { "Content-Type": "application/json" },
 });
 
