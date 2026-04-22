43e51d0b229ebc1e82069a33731566f97b1f004a
nicolzapata <zapatanicol094@gmail.com>
2026-04-22 16:53:02 -0500

    cambios

--- sportwear/src/services/api.js
+++ sportwear/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://surprising-ambition-production-dd12.up.railway.app/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: { "Content-Type": "application/json" },
 });
 
